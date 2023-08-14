1d5d832dd465897d7d56232fee23368bc41b9f1a
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-14 22:03:26 +0530

    create test class and implement the get name function

---
+++ test/user.spec.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(details) {
+    const { firstName, lastName } = details;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
