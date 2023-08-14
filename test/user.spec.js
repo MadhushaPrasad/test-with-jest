6c8aff39c0d13e42aa8bba03d15c52e852c4fd3c
MadhushaPrasad <madushaprasad21@gmail.com>
2023-08-14 22:06:39 +0530

    impl first test case for name function

--- test/user.spec.js
+++ test/user.spec.js
@@ -9,3 +9,10 @@ class User {
     return `${this.firstName} ${this.lastName}`;
   }
 }
+
+describe('User', () => {
+  test('name return full name', () => {
+    const user = new User({ firstName: 'Madhusha', lastName: 'Prasad' });
+    expect(user.name).toBe('Madhusha Prasad');
+  });
+});
