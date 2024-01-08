fb2ac002f175285c26c0d1f1e04c74e541e79492
MadhushaPrasad <madushaprasad21@gmail.com>
2024-01-08 21:38:51 +0530

    snapshot test type 2 (npm test -- -u)

--- test/snapshots.spec.js
+++ test/snapshots.spec.js
@@ -9,15 +9,15 @@ const user = {
 };
 
 test("User matches the snapshot", () => {
-    const userString = `{
-                "name": "Madhusha Prasad",
-                "age": 25,
-                "job": "Software Engineer",
-                "address": {
-                        "city": "Colombo",
-                        "country": "Sri Lanka"
-                }
-        }`;
+    // const userString = `{
+    //             "name": "Madhusha Prasad",
+    //             "age": 25,
+    //             "job": "Software Engineer",
+    //             "address": {
+    //                     "city": "Colombo",
+    //                     "country": "Sri Lanka"
+    //             }
+    //     }`;
 
-    expect(JSON.stringify(user)).toMatchSnapshot(userString);
+    expect(user).toMatchSnapshot();
 });
